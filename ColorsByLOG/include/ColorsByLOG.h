5d75e31fc4e8d4f7ef1938d4c8c544a61b4328e9
LOG <log.plusplus@gmail.com>
2023-10-26 12:09:44 +0300

    first Linux commit

---
+++ ColorsByLOG/include/ColorsByLOG.h
@@ -0,0 +1,9 @@
+#ifndef _COLORS_BY_LOG_
+#define _COLORS_BY_LOG_
+
+#include <iostream>
+#include <map>
+
+void setcolor(std::string);
+
+#endif //_COLORS_BY_LOG_
---
+++ IntroByLOG/include/IntroByLOG.h
@@ -0,0 +1,8 @@
+#ifndef _INTRO_BY_LOG_
+#define _INTRO_BY_LOG_
+
+#include <iostream>
+
+void intro();
+
+#endif //_INTRO_BY_LOG_
